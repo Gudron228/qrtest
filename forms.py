e550b4b703be41a1df371ef69066e54887a739c8
Владимир <vovik190798@gmail.com>
2023-04-15 11:40:23 +0300

    ffff

--- forms.py
+++ forms.py
@@ -4,7 +4,15 @@ from wtforms.validators import DataRequired, ValidationError, EqualTo
 
 
 class QrTextForm(FlaskForm):
+    subject = StringField('Введите тему', validators=[DataRequired()])
     text = TextAreaField('Введите текст:', validators=[DataRequired()])
+    submit = SubmitField('Закодировать')
+
+
+class ContactForm(FlaskForm):
+    first_name = StringField('Введите имя:', validators=[DataRequired()])
+    last_name = StringField('Введите фамилию:', validators=[DataRequired()])
+    phone = StringField('Введите номер телеффона:', validators=[DataRequired()])
     submit = SubmitField('Закодировать')
 
 
--- main.py
+++ main.py
@@ -6,27 +6,42 @@ import cv2
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ljvndkgjns2kj4b23k4jb23k5bj235bj'
 
+menu = [{"name": "Главная", "url": "index"},
+        {"name": "Закодировать техт", "url": "qrtext"},
+        {"name": "Закодировать контакт", "url": "qrcontact"}]
 
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/index', methods=['POST', 'GET'])
+
+@app.route('/')
+@app.route('/index')
 def index():
+    return render_template('index.html', menu=menu)
+
+
+@app.route('/qrtext', methods=['POST', 'GET'])
+def qrtext():
     form = QrTextForm()
     if request.method == 'POST':
-        text = request.form['text']
+        if len(request.form['subject']) + len(request.form['text']) > 600:
+            return redirect(url_for('index'))
+        text = request.form['subject'] + "\n" + request.form['text']
         qr = qrcode.QRCode(
-            version=1,
+            version=None,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
+            box_size=30,
+            border=1,
         )
         qr.add_data(text)
         qr.make(fit=True)
 
         img = qr.make_image(fill_color="black", back_color="white")
-        img.save('image.png')
-        return redirect(url_for('index'))
+        img.save('image1.png')
+        return redirect(url_for('qrtext'))
 
-    return render_template('index.html', form=form)
+    return render_template('qrtext.html', title='Закодировать текст', menu=menu, form=form)
+
+
+@app.route('/qrcontact', methods=['POST', 'GET'])
+def qrcontact():
 
 
 if __name__ == "__main__":
