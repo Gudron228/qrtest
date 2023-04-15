0ecbad338db53f1bef7f099de32d2b21d22eec2a
Владимир <vovik190798@gmail.com>
2023-04-15 14:09:28 +0300

    Добавлены комменты и readme

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import qrcode
 from flask import Flask, render_template, request, redirect, url_for
+import sys
+import vobject
 
 from forms import QrTextForm, ContactForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ljvndkgjns2kj4b23k4jb23k5bj235bj'
-
+# меню для удобного перемещения по страницам
 menu = [{"name": "Главная", "url": "index"},
         {"name": "Закодировать техт", "url": "qr_text"},
         {"name": "Закодировать контакт", "url": "qr_contact"}]
@@ -13,11 +15,13 @@ menu = [{"name": "Главная", "url": "index"},
 
 @app.route('/')
 @app.route('/index')
+#главная страница
 def index():
     return render_template('index.html', menu=menu)
 
 
 @app.route('/qr_text', methods=['POST', 'GET'])
+#страница текстовым qr-кодом
 def qr_text():
     form = QrTextForm()
     if request.method == 'POST':
@@ -41,19 +45,28 @@ def qr_text():
 
 
 @app.route('/qr_contact', methods=['POST', 'GET'])
+#страница с qr-кодом vCard (пока не работает)
 def qr_contact():
     form = ContactForm()
     if request.method == 'POST':
         if len(request.form['first_name']) + len(request.form['last_name']) + len(request.form['phone']) > 600:
             return redirect(url_for('index'))
-        text = request.form['first_name'] + "\n" + request.form['last_name'] + "\n" + request.form['phone']
+        s = f'''BEGIN:VCARD
+        VERSION:3.0
+        N:{request.form['last_name']};{request.form['first_name']}
+        FN:{request.form['last_name']} {request.form['first_name']}
+        ORG:EVenX
+        URL:URL HERE
+        EMAIL:SOME@EMAIL.COM
+        TEL;TYPE=VOICE,WORK,PREF:{request.form['phone']}
+        END:VCARD'''
         qr = qrcode.QRCode(
             version=None,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=30,
             border=1,
         )
-        qr.add_data(text)
+        qr.add_data(s)
         qr.make(fit=True)
 
         img = qr.make_image(fill_color="black", back_color="white")
